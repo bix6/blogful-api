88464330e7c56a429ea3d4ca6f943acc3f04ca68
bix <840573+bix6@users.noreply.github.com>
2020-04-20 08:06:54 -0700

    create migrations 1 and 2

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
-app.use(validateBearerToken);
+//app.use(validateBearerToken);
 
 app.get('/', (req, res) => {
     res.send('Hello, Jello!');
