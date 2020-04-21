91bade89400be78045dd671c96138b725ee9f20f
bix <840573+bix6@users.noreply.github.com>
2020-04-21 07:43:20 -0700

    test POST /articles

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ const ArticlesService = require('./articles-service');
 // const exampleRouter = require('./example-router/example-router');
 
 const app = express();
+const jsonParser = express.json();
 
 const morganOption = (NODE_ENV === 'production')
     ? 'tiny' : 'dev';
@@ -43,6 +44,19 @@ app.get('/articles/:article_id', (req, res, next) => {
                 });
             }
             res.json(article);
+        })
+        .catch(next);
+});
+
+app.post('/articles', jsonParser, (req, res, next) => {
+    const { title, content, style } = req.body;
+    const newArticle = { title, content, style };
+    ArticlesService.insertArticle(req.app.get('db'), newArticle)
+        .then(article => {
+            res
+                .status(201)
+                .location(`/articles/${article.id}`)
+                .json(article)
         })
         .catch(next);
 });
