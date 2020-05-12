0223f33f3ec406b5938fd3199da0768d1990dafe
bix <840573+bix6@users.noreply.github.com>
2020-05-12 16:46:53 -0700

    update DB name

--- src/config.js
+++ src/config.js
@@ -2,5 +2,6 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     API_TOKEN: process.env.API_TOKEN || 'config-api-token',
-    DB_URL: process.env.DB_URL || 'postgresql://dunder_mifflin:hi@localhost/blogful',
+    DATABASE_URL: process.env.DATABASE_URL || 'CHECK CONFIG',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'CHECK CONFIG',
 }
